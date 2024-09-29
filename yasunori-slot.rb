7d89cd6e8898012050b9eafa2a1f6c0e86643b38
Comamoca <comamoca.dev@gmail.com>
2024-09-29 18:42:03 +0900

    :tada: (yasunori-slot.rb) もし出た文字がyasunoriだったらOSを消すよう勧めるメッセージを出す機能を追加

--- yasunori-slot.rb
+++ yasunori-slot.rb
@@ -1,1 +1,21 @@
-puts "yasunori".split("").shuffle.map{|char| ":alphabet-white-#{char}:"}.join("")
+require 'io/console'
+
+terminal_width = IO.console.winsize[1]
+
+def to_emoji(str)
+  # 任意のアルファベットをSlack絵文字にする
+  str.split("").map{|char| ":alphabet-white-#{char}:"}.join("")
+end
+
+shuffled_chars = "yasunori".split("").shuffle.join("")
+emojis = to_emoji(shuffled_chars)
+
+puts emojis
+
+if shuffled_chars == "yasunori" then
+  msgs = ["========================================  Your jackpod!! ========================================",
+   "Let's delete your PC'S Operating System!",
+   "dd if=iso_path of=/dev/your_device bs=4M"]
+
+  msgs.map {|msg| puts msg.center(terminal_width)}
+end
